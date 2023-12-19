5eefe6b2f160878cd3f986225aecdc38d43426c5
ericaostini <ericaostini14@gmail.com>
2023-12-19 09:10:44 +0100

    start nav bar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,14 @@
-<header>header <i class = "fa-solid fa-home"></i></header>
+<div class="d-flex justify-content-center flex-wrap">
+    <li class="m-sm-3 list-unstyled">
+        <a href="#" class="text-dark text-decoration-none">Characters</a>
+        <a href="#" class="text-dark text-decoration-none">Comics</a>
+        <a href="#" class="text-dark text-decoration-none">Movies</a>
+        <a href="#" class="text-dark text-decoration-none">TV</a>
+        <a href="#" class="text-dark text-decoration-none">Games</a>
+        <a href="#" class="text-dark text-decoration-none">Collectibles</a>
+        <a href="#" class="text-dark text-decoration-none">Videos</a>
+        <a href="#" class="text-dark text-decoration-none">Fans</a>
+        <a href="#" class="text-dark text-decoration-none">News</a>
+        <a href="#" class="text-dark text-decoration-none">Shop</a>
+    </li>
+</div>
