94eeb2210435c22c56e5fcbe3ebe6a0bfc444b80
ericaostini <ericaostini14@gmail.com>
2023-12-19 09:51:02 +0100

    style navbar and add form

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,16 @@
 @php
     $navbar = config('db.navbar');
 @endphp
-<div class="d-flex justify-content-center flex-wrap">
-    @foreach ($navbar as $item)
-        <li class="m-sm-3 list-unstyled">
-            <a href="#" class="text-dark text-decoration-none">{{ $item['text'] }}</a>
-        </li>
-    @endforeach
+<div class="container">
+    <div class="d-flex justify-content-between flex-wrap align-items-center">
+        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+        @foreach ($navbar as $item)
+            <li class="m-sm-3 list-unstyled">
+                <a href="#" class="text-dark text-decoration-none">{{ $item['text'] }}</a>
+            </li>
+        @endforeach
+        <div class="mb-3">
+            <input type="text" class="form-control" placeholder="search">
+        </div>
+    </div>
 </div>
