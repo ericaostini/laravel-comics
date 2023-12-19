54ad3f954474412f36d9c59f8ef036098e01ffe7
ericaostini <ericaostini14@gmail.com>
2023-12-19 10:03:18 +0100

    add card image and text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,21 @@
 @section('title', 'Home')
 
 @section('content')
-<main>
-    <h1>Homepage</h1>
-</main>
-    
+    <main>
+        <h1>Homepage</h1>
+        <div class="container">
+            <div class="row">
+                @foreach ($comics as $comic)
+                    <div class="col-12 col-md-4 col-lg-3">
+                        <div class="card">
+                            <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                            <p>{{ $comic['title'] }}</p>
+                        </div>
+
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </main>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $comics = config('db.comics');
     $navbar = config('db.navbar');
-    return view('home');
+    return view('home', compact('comics'));
 });
