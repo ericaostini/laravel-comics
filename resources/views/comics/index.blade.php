a143fdf484a68bc9f512ebca035884662acf008d
ericaostini <ericaostini14@gmail.com>
2023-12-20 15:17:57 +0100

    exercise recovery

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <main>
+        <section class="section-one">
+            <div class="container mt-5">
+                <div class="title-relative text-light fw-bolder">CURRENT SERIES</div>
+            </div>
+        </section>
+        <section class="section-two">
+            <div class="container">
+                <div class="row">
+                    @foreach ($comics as $key => $comic)
+                        <div class="col-12 col-md-3 col-lg-2">
+                            <div>
+                                <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                            </div>
+                            <a href = "{{ route('comics.show', $key) }}"
+                                class="text-light p-3 text-center">{{ $comic['title'] }}</a>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="text-center">
+                    <button class="btn text-light fw-bolder mb-5">LOAD MORE</button>
+                </div>
+            </div>
+        </section>
+    </main>
+
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <main>
+        <section class="section-one">
+            <div class="container mt-5">
+                <div class="title-relative text-light fw-bolder">Comics available</div>
+            </div>
+        </section>
+    </main>
+
+@endsection
